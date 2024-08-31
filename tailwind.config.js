f08097a3b07639c1cf0cc5b473aad3e97aee5196
Hammond ziggy <zorzorisrael6@gmail.com>
2024-08-31 14:22:04 +0100

    feature/Meetings-section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        'custom': '0px 4px 26px 0px #6F6F721F',
+      },
+      colors: {
+        'custom-gray': 'rgba(240, 240, 240, 1)',
+      },
       fontFamily: {
         kanit: ["'Kanit'", "sans-serif"],
+        inter: ["'Inter'", "sans-serif"],
       },
     },
   },
