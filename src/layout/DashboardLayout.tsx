c7b379e8ecc604a71f37b4c02bd4084a8f88b8bb
princedev3 <marvinprince232@gmail.com>
2024-09-07 22:50:34 +0300

    navbar correction

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -5,7 +5,7 @@ export const DashboardLayout = ({ children }: ChildrenOnly) => {
   return (
     <main className="w-full h-screen grid grid-rows-1 lg:grid-cols-[17rem_1fr]">
       <aside className="sidebar-section w-full h-full hidden lg:block bg-white">
-        <Sidebar />
+        <Sidebar desktop={true} />
       </aside>
       <section className="main-content w-full overflow-y-scroll py-[1.875rem] px-5 grid gap-[1.875rem] bg-[#f7f7f7]">
         { children }
